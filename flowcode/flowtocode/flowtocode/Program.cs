ac834bbf9e9d2e76c07d71efddbdf06bc4e06a42
Aishah999 <aishaah@code.edu.az>
2024-02-16 03:21:58 +0400

    flowcode

---
+++ flowcode/flowtocode/flowtocode/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//#region Task1
+//int num = 24;
+//if(num%2==0)
+//{
+//Console.WriteLine("cut");
+//}
+//else
+//{
+//Console.WriteLine("tek");
+//}
+
+
+//# endregion
+
+//#region Task 2
+//int a = 25;
+//if(a%21==0)
+//{
+//Console.WriteLine("bolunur");
+//}
+//else
+//{
+//Console.WriteLine("bolunmur");
+//}
+
+//#endregion
+
+//#region Task 3
+//int x = 14;
+//int y = 18;
+//t result = 0;
+//if(x%2==0&&y%2==0)
+//{
+//result = x + y;
+//}
+//else
+//{
+//result = x - y;
+//}
+
+//Console.WriteLine(result);
+
+//#endregion
+#region Task 4
+double temp = 18.5;
+if(temp>15)
+{
+    Console.WriteLine("isti");
+
+}
+else if(temp==15)
+{
+    Console.WriteLine("15dir");
+}
+else
+{
+    Console.WriteLine("soyuqdur");
+}
+
+
+
+
+
+#endregion
